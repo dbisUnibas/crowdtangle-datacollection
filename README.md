3dbf5e55ee9b56aeb0f4ea1aabab01887652e597
Simon Peterhans <simon.peterhans@gmail.com>
2022-07-21 14:00:52 +0200

    Clean up and extend functionality to fetch all images (not just posts tagged as image).

--- README.md
+++ README.md
@@ -1,18 +1,37 @@
 # crowdtangle-datacollection
 
-Simple python script to download images received by the Crowdtangle API (Facebook or Instagram).
+Simple python script to download posts containing images received by the CrowdTangle API (Facebook or Instagram).
 
 ## Requirements
-pip install -r requirements.txt
+
+- Tested on Python 3.8
+- `pip3 install -r requirements.txt`
+- CrowdTangle API keys for Facebook and/or Twitter (to be entered in `apirequest.py`)
 
 ## Usage
 
+Either `-f` (Facebook) or `-i` (Instagram) must be provided as command line argument.
+
+The targeted lists have to be provided with `-o "listIds=<ID_1,ID_2[,ID_I]>"`.
+
+Two modes are supported:
+
+- Obtaining images from a given starting date until now (`-s`)
+- Obtaining images periodically in time intervals every N days (`-t`)
+
 ### Download images since some specified date
-`python apirequest.py -f/-i (Facebook or Instagram) -o "listIds="<someListID>" -s "<start date>"`
 
-e.g. `python apirequest.py -f -o "listIds=1568443,1568444" -s "2015-01-01"`
+`python3 apirequest.py [-f|-i] -o "listIds="<someListID>" -s "<start date>"`
 
-### Run script continuously every specified timeinterval
-`python apirequest.py -f/-i (Facebook or Instagram) -o "listIds="<someListID>" -t <time_interval>`
+Example: `python3 apirequest.py -f -o "listIds=1568443,1568444" -s "2021-12-31"`
 
-e.g. `python apirequest.py -f -o "listIds=1568443,1568444" -t 4`
+This request would download images and posts since the 31st of December, 2021.
+
+### Download images in time intervals every N days
+
+`python3 apirequest.py [-f|-i] -o "listIds="<someListID>" -t <time_interval_days>`
+
+Example: `python3 apirequest.py -f -o "listIds=1568443,1568444" -t 3`
+
+This request would download new posts every 3 days until the script is stopped.  
+The first interval is executed immediately, meaning that the first posts and images obtained are up to N days old.
--- apirequest.py
+++ apirequest.py
@@ -10,81 +10,74 @@ import config
 import requests
 
 
-@limits(calls=5, period=1)
+@limits(calls=5, period=60, raise_on_limit=False)
 def api_call(request_url):
+    print(request_url)
     return requests.get(request_url).json()
 
 
 # Loop through result and save images in account folders
 def retrieve_images_for_result(json_result, platform):
     for post_item in json_result['posts']:
-        account_name = "".join([c for c in post_item['account']['name']
-                                if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
-        account_id = post_item['account']['platformId']
-
+        # Since CrowdTangle never provides more than 1 image, this should be irrelevant.
         i = 0
-        if post_item['type'] == 'photo':
 
-            # Create directory for each account in photo results
-            if not os.path.exists(platform + "/" + account_name + "-" + account_id):
-                os.mkdir(platform + "/" + account_name + "-" + account_id)
+        for media_item in post_item['media']:
+            if media_item['type'] == 'photo':
+                # Create directory for each account in photo results.
+                account_name = "".join([c for c in post_item['account']['name']
+                                        if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
+                account_id = post_item['account']['platformId']
 
-            for media_item in post_item['media']:
-                if media_item['type'] == 'photo':
-                    link = ''
-                    if platform == 'facebook':
-                        if 'full' in media_item:
-                            link = media_item['full']
-                    else:
-                        if 'url' in media_item:
-                            link = media_item['url']
+                if not os.path.exists(platform + "/" + account_name + "-" + account_id):
+                    os.mkdir(platform + "/" + account_name + "-" + account_id)
 
-                    # Download image, filename is timestamp of post
-                    filename = platform + "/" + account_name + "-" + account_id + "/" + "".join(
-                        [c for c in post_item['date'] if c.isalpha() or c.isdigit() or c == ' ']).rstrip() + str(i)
+                link = media_item['url']
 
-                    with open(filename + ".json", "w", encoding="utf-8") as f:
-                        json.dump(post_item, f, ensure_ascii=False, indent=4)
+                # Download image, filename is timestamp of post
+                filename = platform + "/" + account_name + "-" + account_id + "/" + "".join(
+                    [c for c in post_item['date'] if c.isalpha() or c.isdigit() or c == ' ']).rstrip() + ' ' + str(i)
 
-                    if link != '':
-                        if not os.path.exists(filename):
-                            try:
-                                request.urlretrieve(link, filename + ".jpg")
-                                print('Image ' + post_item['date'] + str(i) + '.jpg saved!')
-                            except error.HTTPError:
-                                print('Image ' + post_item['date'] + str(i) + '.jpg could not be downloaded.')
-                                print(link)
-                        i += 1
+                # Write post
+                with open(filename + ".json", "w", encoding="utf-8") as f:
+                    json.dump(post_item, f, ensure_ascii=False, indent=4)
+
+                if link != '':
+                    if not os.path.exists(filename):
+                        try:
+                            request.urlretrieve(link, filename + ".jpg")
+                            print('Image ' + post_item['date'] + str(i) + '.jpg saved!')
+                        except error.HTTPError:
+                            print('Image ' + post_item['date'] + str(i) + '.jpg could not be downloaded.')
+                            print(link)
+                i += 1
 
 
 def paginate_request(platform, url, request_name):
-    print(url)
     api_response = api_call(url)
 
-    # Check if request was successful
-    if api_response['status'] == 200:
-        results = api_response["result"]
-
-        # Loop through pages of response
-        while api_response['status'] == 200 and 'nextPage' in api_response['result']['pagination']:
-            api_response = api_call(api_response['result']['pagination']['nextPage'])
-            results.update(api_response["result"])
-            print("Waiting 10 seconds before next request...")
-            time.sleep(10)
-
+    # Loop through pages of response
+    while api_response['status'] == 200:
         if not os.path.exists(platform):
             os.mkdir(platform)
 
+        # This is just the last (paginated) request made and overrides the last dump for the same query.
         with open(platform + "/" + request_name + ".json", "w", encoding="utf-8") as f:
-            json.dump(results, f, ensure_ascii=False, indent=4)
+            json.dump(api_response["result"], f, ensure_ascii=False, indent=4)
 
         print("API-Response saved!")
-        last_execution_date = datetime.now()
-        retrieve_images_for_result(results, platform)
+        retrieve_images_for_result(api_response["result"], platform)
 
-    else:
-        print("Error: " + str(api_response['status']) +
-              ". See https://github.com/CrowdTangle/API/wiki/Errors for help.")
+        if 'nextPage' not in api_response['result']['pagination']:
+            break
+
+        next_request_url = api_response['result']['pagination']['nextPage']
+        api_response = api_call(next_request_url)
+
+    if api_response['status'] != 200:
+        print(
+            "Error: " + str(api_response['status']) + " - See https://github.com/CrowdTangle/API/wiki/Errors for help."
+        )
 
 
 def main():
@@ -92,18 +85,18 @@ def main():
     platform = 'facebook'
     token = config.facebook_token
     parameters = ''
-    time_interval = 86400 * 5
+    time_interval = ''
     start_date = ''
 
     # Parse arguments
     try:
-        opts, args = getopt.getopt(argv, "hfio:t:s:, [options=, timeinterval=, startdate=]")
+        opts, args = getopt.getopt(argv, "hfio:t:s:, [options=, time-interval=, startdate=]")
     except getopt.GetoptError:
-        print('apirequest.py -i/-f -o <parameters> -t <timeinterval in days> (4 days are safe)')
+        print('apirequest.py -i/-f -o <parameters> -t <time-interval in days> (4 days are safe)')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('apirequest.py -i/-f -o <parameters> -t <timeinterval in days> (4 days are safe)')
+            print('apirequest.py -i/-f -o <parameters> -t <time-interval in days> (4 days are safe)')
             sys.exit()
         elif opt == "-f":
             print('Facebook request')
@@ -116,47 +109,50 @@ def main():
         elif opt in ('-o', '--options'):
             print("Parameters: " + arg)
             parameters = arg
-        elif opt in ('-t', '--timeinterval'):
-            print("Timeinterval: " + arg + " days")
-            if time_interval == '1':
-                time_interval = 2
-            else:
-                time_interval = arg
-        elif opt in ('-s', '--startdate'):
+        elif opt in ('-t', '--time-interval'):
+            print("Time interval: " + arg + " days")
+            time_interval = arg
+        elif opt in ('-s', '--start-date'):
             print("Start date: " + arg)
             start_date = arg
 
-    last_execution_date = datetime.today() - timedelta(days=int(time_interval))
-
-    url = "https://api.crowdtangle.com/posts"
-    request_name = platform + "-request-" + datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+    base_path = "https://api.crowdtangle.com/posts"
 
     if start_date != '':
-        end = datetime.today()
-        start = end - timedelta(days=10)
+        end = datetime.now()
+        start = datetime.strptime(start_date, '%Y-%m-%d')
         i = 0
-        while datetime.strptime(start_date, '%Y-%m-%d') <= end:
-            request_name = platform + "-request-" + datetime.now().strftime("%d-%m-%Y-%H-%M-%S") + start + end
-            url = "https://api.crowdtangle.com/posts?token=" + token + "&sortBy=date&count=100&" + parameters + "&startDate=" + start.strftime(
-                "%Y-%m-%d") + "&endDate=" + end.strftime("%Y-%m-%d")
-            end = end - timedelta(days=20)
-            start = start - timedelta(days=20)
+        while datetime.strptime(start_date, '%Y-%m-%d') < end:
+            request_name = platform + "-request-" + datetime.now().strftime("%d-%m-%Y-%H-%M-%S") + str(start) + str(end)
+            url = base_path + "?token=" + token + "&sortBy=date&count=100&" + parameters \
+                  + "&startDate=" + start.strftime("%Y-%m-%dT%H:%M:%S") \
+                  + "&endDate=" + end.strftime("%Y-%m-%dT%H:%M:%S")
             paginate_request(platform, url, request_name + str(i))
+            end = start
+            start = start - timedelta(days=1)
             i += 1
 
-    else:
-        request_name = platform + "-request-" + datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
-        # Build request url
-        url = url + "?token=" + token + "&sortBy=date&count=100&" + parameters + "&startDate=" + (
-                last_execution_date + timedelta(days=1)).strftime("%Y-%m-%d") + "&endDate=" + datetime.today().strftime(
-            "%Y-%m-%d")
+    if time_interval != '':
+        time_interval = int(time_interval)
+        last_execution_date = datetime.now() - timedelta(days=time_interval)
 
         # Perform request every 'time_interval' days
         while True:
-            if not datetime.today() == last_execution_date:
-                paginate_request(platform, url, request_name)
+            current_execution_date = datetime.now()
+            request_name = platform + "-request-" + current_execution_date.strftime("%Y-%m-%dT%H:%M:%S")
+
+            # Build request url
+            url = base_path + "?token=" + token + "&sortBy=date&count=100&" + parameters \
+                  + "&startDate=" + last_execution_date.strftime("%Y-%m-%dT%H:%M:%S") \
+                  + "&endDate=" + current_execution_date.strftime("%Y-%m-%dT%H:%M:%S")
+
+            paginate_request(platform, url, request_name)
+
+            last_execution_date = current_execution_date
+
             print("Waiting until next timed request.")
-            time.sleep(86400 * int(time_interval))
+
+            time.sleep(86_400 * time_interval)
 
 
 if __name__ == "__main__":
